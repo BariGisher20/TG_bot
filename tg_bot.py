c943d640f64fc3428e1e5eb6c36071398e85c6d0
Eleonora Harutyunyan <ev.arutyunyan@mail.ru>
2022-05-02 14:55:07 +0300

    first commit

---
+++ tg_bot.py
@@ -0,0 +1,38 @@
+import telebot
+from telebot import types
+import request
+
+
+token = "5350268880:AAGt6VZ5xTdLKLNUFcuse0HA37Jv9oZ8PXc"
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Привет ✌️ ")
+
+
+@bot.message_handler(commands=['button'])
+def button_message(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Кнопка")
+    markup.add(item1)
+    bot.send_message(message.chat.id, 'Выберите что вам надо', reply_markup=markup)
+
+
+@bot.message_handler(content_types='text')
+def message_reply(message):
+    if message.text == "Кнопка":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton("Кнопка 2")
+        markup.add(item1)
+        bot.send_message(message.chat.id, 'Выберите что вам надо', reply_markup=markup)
+    elif message.text == "Кнопка 2":
+        bot.send_message(message.chat.id, 'Спасибо за прочтение статьи!')
+
+
+bot.infinity_polling()
+
+
+
+bot.infinity_polling()
